683f22beb0e2f63ac7cc43b578a625b87531abc4
Marko Vasev <Marko.Vasev@gmail.com>
2022-12-03 13:37:49 +0800

    cleanup experiments

--- Calendar/client/aligner/src/App.jsx
+++ Calendar/client/aligner/src/App.jsx
@@ -1,7 +1,33 @@
 import { useState } from "react";
 import AlignWeek from "./views/AlignWeek";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 import "./App.css";
 
+const client = new ApolloClient({
+  uri: "https://flyby-gateway.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 function App() {
   const [count, setCount] = useState(0);
 
